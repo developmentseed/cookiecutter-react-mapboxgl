d3c016aeef176064ea71b42704b7af9ed727a08f
Alice Rühl <alice@ds.io>
2021-02-18 16:43:44 +0100

    Update post_gen_project.py

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -3,6 +3,6 @@ import os
 
 os.system('cp .env.example .env')
 os.system('yarn install')
-os.system('git init -b develop')
+os.system('git init')
 os.system('git add .')
 os.system('git commit -m "Setup project" -m "using https://github.com/developmentseed/cookiecutter-react-mapboxgl"')
